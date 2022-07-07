cf701a22ca9b6330f50519c1456e965b1ac8dddd
Amaury <ricardo901130@gmail.com>
2022-07-07 12:13:43 -0300

    add resources

---
+++ lib/resources/images.dart
@@ -0,0 +1,116 @@
+
+part of 'resources.dart';
+/// Add images references here
+/// Call:
+/// Images.app_logo.get()
+/// **/
+enum Images {
+  app_logo,
+}
+
+extension LoadImage on Images {
+  static const assetsFolder = "assets";
+  static const imagesResFolder = "$assetsFolder/images";
+  static const icResFolder = "$imagesResFolder/icons";
+  static const svgResFolder = "$imagesResFolder/svg";
+
+  static final Map<Images, String> _imagesValues = {
+    Images.app_logo: '$icResFolder/logo.png',
+  };
+
+  String get value => _imagesValues[this]!;
+
+  Widget svg({
+    double? width,
+    double? height,
+    BoxFit fit = BoxFit.contain,
+    Color? color,
+    Alignment alignment = Alignment.center,
+    String? semanticLabel,
+  }) =>
+      _SvgImage.asset(
+        value,
+        width: width,
+        height: height,
+        fit: fit,
+        color: color,
+        alignment: alignment,
+        semanticLabel: semanticLabel,
+      );
+
+  Widget get({
+    Key? key,
+    double? scale,
+    double? width,
+    double? height,
+    BoxFit fit = BoxFit.contain,
+    Color? color,
+    Alignment alignment = Alignment.center,
+    String? semanticLabel,
+  }) {
+    if (value.endsWith("svg")) {
+      return svg(
+        width: width,
+        height: height,
+        fit: fit,
+        color: color,
+        alignment: alignment,
+        semanticLabel: semanticLabel,
+      );
+    }
+
+    return get(
+      key: key,
+      scale: scale,
+      width: width,
+      height: height,
+      fit: fit,
+      alignment: alignment,
+    );
+  }
+
+  Image img({
+    Key? key,
+    double? scale,
+    double? width,
+    double? height,
+    BoxFit fit = BoxFit.contain,
+    Alignment alignment = Alignment.center,
+  }) =>
+      Image.asset(
+        value,
+        bundle: assetImage().bundle,
+        height: width,
+        width: width,
+        scale: scale,
+        fit: fit,
+        alignment: alignment,
+      );
+
+  AssetImage assetImage() => AssetImage(value);
+}
+
+class _SvgImage {
+  static Widget asset(String assetPath, {
+    double? width,
+    double? height,
+    BoxFit fit = BoxFit.contain,
+    Color? color,
+    Alignment alignment = Alignment.center,
+    String? semanticLabel,
+    String? package,
+    @visibleForTesting bool isWeb = false,
+  }) {
+    return SvgPicture.asset(
+      assetPath,
+      key: Key(assetPath),
+      width: width,
+      height: height,
+      fit: fit,
+      color: color,
+      alignment: alignment,
+      semanticsLabel: semanticLabel,
+      package: package,
+    );
+  }
+}
